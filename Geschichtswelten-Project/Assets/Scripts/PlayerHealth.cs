1cdd330b80ad6fce575501da6156ae6121d5e534
PerpAzul <felipe.lucas@outlook.com>
2023-12-11 17:45:26 +0100

    Player Health

--- Geschichtswelten-Project/Assets/Scripts/PlayerHealth.cs
+++ Geschichtswelten-Project/Assets/Scripts/PlayerHealth.cs
@@ -1,25 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public float currentHealth;
     public int maxHealth = 3;
 
+    public Image overlay;
+    public float duration;
+    public float fadeSpeed;
+    private float durationTimer;
 
     void Start()
     {
         currentHealth = maxHealth;
+        overlay.color = new Color(overlay.color.r, overlay.color.g, overlay.color.b, 0);
     }
     
     void Update()
     {
-        
+        if (overlay.color.a > 0)
+        {
+            durationTimer += Time.deltaTime;
+            if (durationTimer > duration)
+            {
+                float tempAlpha = overlay.color.a;
+                tempAlpha -= Time.deltaTime * fadeSpeed;
+                overlay.color = new Color(overlay.color.r, overlay.color.g, overlay.color.b, tempAlpha);
+            }
+        }
     }
 
     public void TakeDamage()
     {
         currentHealth--;
+        durationTimer = 0;
+        overlay.color = new Color(overlay.color.r, overlay.color.g, overlay.color.b, 0.5f);
     }
 }
