c40131edd9b3756a8c1dae245c6ff0237c274f6c
THEderzport <thederzport@gmail.com>
2023-12-14 23:05:38 +0100

    Updated Prefabs for picking up stuff and picking up stuff works ish (Need to fix it so it moves up and down)

--- Geschichtswelten-Project/Assets/Scripts/PlayerLook.cs
+++ Geschichtswelten-Project/Assets/Scripts/PlayerLook.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 
 public class PlayerLook : MonoBehaviour
@@ -15,8 +16,9 @@ public class PlayerLook : MonoBehaviour
     private bool _useGravityFloat;
     private Rigidbody _turnoff;
     private bool Stop;
+    [SerializeField] private Transform hold;
+    private RaycastHit PickUpHit;
 
-        
 
     //GravityPush Power with Collision Detection
     public void GravityPush()
@@ -36,7 +38,6 @@ public class PlayerLook : MonoBehaviour
                     Debug.Log(distance);
                     switch (distance)
                     {
-                        
                         case <= 5f:
                             rigidbody.AddForce((rigidbody.transform.position - transform.position).normalized * 7500f,
                                 ForceMode.Force);
@@ -50,6 +51,7 @@ public class PlayerLook : MonoBehaviour
                                 ForceMode.Force);
                             break;
                     }
+
                     //Cooldown
                     StartCoroutine(StartCountdown(5));
                 }
@@ -82,7 +84,7 @@ public class PlayerLook : MonoBehaviour
                     Debug.Log("Float hit!");
                     _turnoff = hitGameObject.GetComponent<Rigidbody>();
                     _turnoff.useGravity = false;
-                    _turnoff.AddForce(Vector3.up.normalized * 15f, ForceMode.Force);
+                    _turnoff.AddForce(Vector3.up.normalized * Time.deltaTime * 15f, ForceMode.Force);
                     _turnoff.GetComponent<SphereCollider>().radius = 3.02f;
                     _turnoff.GetComponent<SphereCollider>().enabled = true;
                     _useGravityFloat = true;
@@ -104,13 +106,16 @@ public class PlayerLook : MonoBehaviour
         Debug.Log("No Float Hit");
     }
 
-    //Can Pick Up Stuff but does not move with Player 
+
+    //Can Pick Up Stuff but now it does not Rotate with the Camera Upwards again will fix later
     public void PickUpStuff()
     {
+        
         if (!Stop)
         {
             RaycastHit hit;
-            if (Physics.Raycast(cam.transform.position, cam.transform.TransformDirection(Vector3.forward), out hit,  Mathf.Infinity))
+            if (Physics.Raycast(cam.transform.position, cam.transform.TransformDirection(Vector3.forward), out hit,
+                    Mathf.Infinity))
             {
                 var hitGameObject = hit.collider.gameObject;
                 if (!hitGameObject.CompareTag("Untagged") && hitGameObject.GetComponent<Rigidbody>())
@@ -118,24 +123,23 @@ public class PlayerLook : MonoBehaviour
                     //Place it slighty above
                     hitGameObject.GetComponent<Rigidbody>().useGravity = false;
                     hitGameObject.GetComponent<Rigidbody>().drag = 10;
-                    hitGameObject.GetComponent<Rigidbody>().freezeRotation = true;
                     Stop = true;
+                    hitGameObject.GetComponent<Rigidbody>().transform.parent = hold;
+                    hitGameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
                     var moveDirection = (cam.transform.position - hitGameObject.transform.position);
-                    hitGameObject.GetComponent<Rigidbody>().AddForce(moveDirection * 150.0f);
+                    hitGameObject.GetComponent<Rigidbody>().AddForce(moveDirection * 250.0f);
+                    hitGameObject.transform.rotation = cam.transform.rotation;
+                    PickUpHit = hit;
                 }
             }
         }
         else
         {
-            RaycastHit hit;
-            if (Physics.Raycast(cam.transform.position, cam.transform.TransformDirection(Vector3.forward), out hit,
-                    Mathf.Infinity))
-            {
-                hit.collider.gameObject.GetComponent<Rigidbody>().useGravity = true;
-                hit.collider.gameObject.GetComponent<Rigidbody>().drag = 0;
-                hit.collider.gameObject.GetComponent<Rigidbody>().freezeRotation = false;
-                Stop = false;
-            }
+            PickUpHit.collider.gameObject.GetComponent<Rigidbody>().useGravity = true;
+            PickUpHit.collider.gameObject.GetComponent<Rigidbody>().drag = 1;
+            PickUpHit.collider.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+            PickUpHit.collider.gameObject.GetComponent<Rigidbody>().transform.parent = null;
+            Stop = false;
         }
     }
 
@@ -157,7 +161,8 @@ public class PlayerLook : MonoBehaviour
                     var rigidbody = hitGameObject.GetComponent<Rigidbody>();
                     var distance = transform.position - rigidbody.transform.position;
                     rigidbody.AddForce(
-                        (transform.position - rigidbody.transform.position).normalized + distance * 75.75f,
+                        (transform.position - rigidbody.transform.position).normalized * Time.deltaTime +
+                        distance * 75.75f,
                         ForceMode.Force);
                     StartCoroutine(StartCountdown(5));
                 }
