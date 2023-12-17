2823755d05ffe99d25c298f25c3a9bb7646fb693
THEderzport <thederzport@gmail.com>
2023-12-17 18:46:21 +0100

    Added the free sound when the door opens

--- Geschichtswelten-Project/Assets/Scripts/Puzzles/OpenDoor.cs
+++ Geschichtswelten-Project/Assets/Scripts/Puzzles/OpenDoor.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -8,16 +9,32 @@ public class OpenDoor : MonoBehaviour
     [SerializeField] private PressurePlate pressurePlate1;
     [SerializeField] private PressurePlate pressurePlate2; 
     public Animator animator;
+    public AudioClip _clip;
+    public AudioSource _Source;
+    private bool canPlay;
+
+  
 
     private void Update()
     {
         if (pressurePlate1.isActivated && pressurePlate2.isActivated || pressurePlate2.isActivated && pressurePlate1.isActivated)
         {
             animator.SetBool("OpenDoor",true);
+            PlaySound();
         }
         else
         {
             animator.SetBool("OpenDoor",false);
+            canPlay = false;
+        }
+    }
+
+    private void PlaySound()
+    {
+        if (!canPlay)
+        {
+            _Source.PlayOneShot(_clip);
+            canPlay = true;
         }
     }
 }
