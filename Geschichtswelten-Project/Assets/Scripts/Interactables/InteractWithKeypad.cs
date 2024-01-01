738ea7e0f5897afd55d37c718e6274835e4b0fc9
THEderzport <thederzport@gmail.com>
2024-01-01 16:30:45 +0000

    All Camera Bugs Fixed

--- Geschichtswelten-Project/Assets/Scripts/Interactables/InteractWithKeypad.cs
+++ Geschichtswelten-Project/Assets/Scripts/Interactables/InteractWithKeypad.cs
@@ -46,13 +46,13 @@ public class InteractWithKeypad : Interactable
 
     protected override void Interact()
     {
+        _KeypadCam.Priority = 1;
+        mainCamera.Priority = 0;
         _KeypadCam.enabled = true;
         _KeypadCam.GetComponent<Camera>().enabled = true;
         mainCamera.enabled = false;
         Weapons.gameObject.SetActive(false);
         hitMarker.gameObject.SetActive(false);
-        _KeypadCam.Priority = 1;
-        mainCamera.Priority = 0;
         player.gameObject.SetActive(false);
         GetComponent<BoxCollider>().enabled = false;
         Cursor.visible = true;
