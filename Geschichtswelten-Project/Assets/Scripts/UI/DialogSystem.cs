2c38a272e439a25b67266f81258d7d9d84057de9
Gewinner2170 <peikvincent@gmail.com>
2024-01-04 22:11:28 +0100

    Dialog system stops on pause menu
    This works by accessing the pause managers "isPaused" value. However, there are some edge cases were the dialog might not stay around for the intended 2 seconds, when the player is e.g. only shortly unpausing the game and then going back to the pause menu

--- Geschichtswelten-Project/Assets/Scripts/UI/DialogSystem.cs
+++ Geschichtswelten-Project/Assets/Scripts/UI/DialogSystem.cs
@@ -9,7 +9,9 @@ public class DialogSystem : MonoBehaviour
 {
 //based on this yt tutorial: https://youtu.be/8oTYabhj248?si=JnXvAmiuMDO14vIT and adapted
     [SerializeField] private TextMeshProUGUI textComponent;
-
+    [SerializeField] private GameObject pauseManager;
+    private PauseMenu pauseMenuScript;
+    
     private List<string> dialogLines;
 
     [SerializeField] private float textDelay; //lower values mean faster typing of the words on screen
@@ -24,6 +26,7 @@ public class DialogSystem : MonoBehaviour
     {
        // the Dialog System should start disabled. Can easily be changed
        this.gameObject.SetActive(false);
+       pauseMenuScript = pauseManager.GetComponent<PauseMenu>();
     }
 
     // Update is called once per frame
@@ -56,17 +59,25 @@ public class DialogSystem : MonoBehaviour
         currentlyTyping = true;
         foreach (char c in dialogLines[curIndex])
         {
+            yield return new WaitUntil(() => pauseMenuScript.isPaused == false); //pauses if the game is paused until the game is no longer paused
             textComponent.text += c;
             yield return new WaitForSeconds(textDelay);
         }
         //text stays around for a bit longer before next text block appears to give the reader time to catch up
         yield return new WaitForSeconds(timeBeforeNextLine);
+        if (pauseMenuScript.isPaused) //in the case that the player pressed pause during the linger time and the time passed, they get another one once they are back in the game. This could still be problematic, e.g. if the player pauses then briefly unpauses and pauses again
+        {
+            yield return new WaitUntil(() => pauseMenuScript.isPaused == false); //pauses if the game is paused until the game is no longer paused
+            yield return new WaitForSeconds(timeBeforeNextLine);
+        }
+
         currentlyTyping = false;
         
         if(curIndex == dialogLines.Count - 1)
         {
-            //giving some extra time on the last one (could be unnecessary)
+            //giving some extra time on the last one (potentially unnecessary)
             yield return new WaitForSeconds(timeBeforeNextLine);
+            
             //no more need for the Dialog box currently, so it will disappear from the screen
             gameObject.SetActive(false);
         }
