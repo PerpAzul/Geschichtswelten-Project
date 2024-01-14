a7bdabdfc624a0322f7996a37b9ab790b359d312
PerpAzul <felipe.lucas@outlook.com>
2024-01-14 21:50:25 +0100

    small changes + bug fixes

--- Geschichtswelten-Project/Assets/Scripts/Puzzles/OpenDoorButton.cs
+++ Geschichtswelten-Project/Assets/Scripts/Puzzles/OpenDoorButton.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class OpenDoorButton : Interactable
 {
+    [SerializeField] private GameObject button;
+    
     protected override void Interact()
     {
-        
+        button.gameObject.GetComponent<BoxCollider>().enabled = false;
     }
 }
