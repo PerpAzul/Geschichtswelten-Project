9b08502656e4d1d39aceca74bed07100d260ee43
PerpAzul <felipe.lucas@outlook.com>
2024-01-14 14:33:32 +0100

    Laboratory puzzle

---
+++ Geschichtswelten-Project/Assets/Scripts/Interactables/InteractTubes.cs
@@ -0,0 +1,129 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class InteractTubes : Interactable
+{
+    [SerializeField] private GameObject tube1;
+    [SerializeField] private GameObject tube2;
+    [SerializeField] private GameObject tube3;
+    [SerializeField] private GameObject tube4;
+    [SerializeField] private GameObject tube5;
+    [SerializeField] private GameObject tube6;
+    [SerializeField] private int index;
+
+    protected override void Interact()
+    {
+        if (index == 2)
+        {
+            Change2Tubes();
+        }
+        else if (index == 3)
+        {
+            Change3Tubes();
+        }
+        else if (index == 4)
+        {
+            Change4Tubes();
+        }
+        else if (index == 7)
+        {
+            Reset();
+        }
+    }
+
+    public void Change2Tubes()
+    {
+        if (tube1.gameObject.activeSelf)
+        {
+            tube1.SetActive(false);
+        }
+        else
+        {
+            tube1.SetActive(true);
+        }
+
+        if (tube2.gameObject.activeSelf)
+        {
+            tube2.SetActive(false);
+        }
+        else
+        {
+            tube2.SetActive(true);
+        }
+    }
+
+    public void Change3Tubes()
+    {
+        if (tube1.gameObject.activeSelf)
+        {
+            tube1.SetActive(false);
+        }
+        else
+        {
+            tube1.SetActive(true);
+        }
+
+        if (tube2.gameObject.activeSelf)
+        {
+            tube2.SetActive(false);
+        }
+        else
+        {
+            tube2.SetActive(true);
+        }
+        
+        if (tube3.gameObject.activeSelf)
+        {
+            tube3.SetActive(false);
+        }
+        else
+        {
+            tube3.SetActive(true);
+        }
+    }
+
+    public void Change4Tubes()
+    {
+        if (tube1.gameObject.activeSelf)
+        {
+            tube1.SetActive(false);
+        }
+        else
+        {
+            tube1.SetActive(true);
+        }
+
+        if (tube2.gameObject.activeSelf)
+        {
+            tube2.SetActive(false);
+        }
+        else
+        {
+            tube2.SetActive(true);
+        }
+        
+        if (tube3.gameObject.activeSelf)
+        {
+            tube3.SetActive(false);
+        }
+        else
+        {
+            tube3.SetActive(true);
+        }
+        
+        tube4.SetActive(false);
+    }
+
+    public void Reset()
+    {
+        tube1.SetActive(false);
+        tube2.SetActive(false);
+        tube3.SetActive(false);
+        tube4.SetActive(false);
+        tube5.SetActive(false);
+        tube6.SetActive(false);
+    }
+}
