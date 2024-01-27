8b487fcd0cbc63a096faa470d626982ff2f63c5d
Gewinner2170 <peikvincent@gmail.com>
2024-01-27 14:16:38 +0100

    soundSetting is persistent now
    using PlayerPref to save the Sound volume between scenes and also beyond(so if the player closes the game and reopens it later)

--- Geschichtswelten-Project/Assets/Scripts/Music/VolumeAdjuster.cs
+++ Geschichtswelten-Project/Assets/Scripts/Music/VolumeAdjuster.cs
@@ -1,11 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class VolumeAdjuster : MonoBehaviour
 {
     [SerializeField] AudioMixer audioMixer;
+    [SerializeField] private Slider volumeSlider;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("volume"))
+        {
+            volumeSlider.value = PlayerPrefs.GetFloat("volume");
+        }
+    }
 
     //called on Value changed by the slider. Also based on this video: https://www.youtube.com/watch?v=YOaYQrN1oYQ
     public void SetVolume(float newVolume)
@@ -13,5 +24,6 @@ public class VolumeAdjuster : MonoBehaviour
         //TODO 
         Debug.Log(newVolume);
         audioMixer.SetFloat("volume", newVolume);
+        PlayerPrefs.SetFloat("volume", newVolume);
     }
 }
